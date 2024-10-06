b98923c1a2908208c07b94389272435cd86e0b2e
Parthsarthi Joshi <parthucmas@gmail.com>
2024-10-06 23:37:37 +0530

    Update 1 Data Validation Component

--- training/components/data_ingestion.py
+++ training/components/data_ingestion.py
@@ -47,9 +47,10 @@ class DataIngestion:
             raise CustomException(e, sys)
         
 
-if __name__ == "__main__":
+# To test the component
+"""if __name__ == "__main__":
     config = ConfigurationManager()
     data_ingestion_config = config.get_data_ingestion_config()
 
     data_ingestion = DataIngestion(config=data_ingestion_config)
-    data_ingestion.save_data()
+    data_ingestion.save_data()"""
