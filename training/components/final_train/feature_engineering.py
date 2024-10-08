0f0f2264be54ef783dd28e00ae77d5015f0a34b4
Parthsarthi Joshi <parthucmas@gmail.com>
2024-10-08 20:52:01 +0530

    Changes in feature_eng.py

--- training/components/final_train/feature_engineering.py
+++ training/components/final_train/feature_engineering.py
@@ -30,30 +30,9 @@ class FeatureEngineering:
         logging.info(f"{37} Dtype of X_train: {X_train.dtype} and X_test is {X_test.dtype}")
         logging.info(f"{38} Dtype of y_train: {y_train.dtype} and y_test is {y_test.dtype}")
 
-        self.check_transformed_already_exists()
+        return self.check_transformed_already_exists(X_train,X_test, y_train, y_test, groups_train)
 
-
-    def check_transformed_already_exists(self):
-      if os.path.exists(os.path.join(self.config.root_dir,"Train.npz")):
-          if os.path.exists(os.path.join(self.config.root_dir,"Test.npz")):
-              logging.info("Features ALREADY transformed")
-              transformed_data_path = self.config.root_dir
-              train_data = np.load(train_data_path)
-              test_data = np.load(test_data_path)
-
-              X_train, y_train, groups_train = train_data["X_train"], train_data["y_train"], train_data["groups_train"]
-              X_test, y_test = test_data["X_test"], test_data["y_test"]
-              
-              with open(self.config.STATUS_FILE,"w") as f:
-                f.write(f"Feature engineering completed {True}")
-              return X_train, X_test, y_train, y_test, groups_train
-          else:
-              self.proceed_with_feature_engineering()
-
-      else:
-          self.proceed_with_feature_engineering()
-    
-      def proceed_with_feature_engineering(self):
+    def proceed_with_feature_engineering(self, X_train, X_test, y_train, y_test, groups_train):
         logging.info("Features are now BEING transformed")
         transform_pipeline = Pipeline([
                   ("scaler", StandardScaler()),
@@ -69,6 +48,30 @@ class FeatureEngineering:
             f.write(f"Feature engineering completed {True}")
             
         return X_train, X_test, y_train, y_test, groups_train
+    
+    def check_transformed_already_exists(self, X_train, X_test, y_train, y_test, groups_train):
+      if os.path.exists(os.path.join(self.config.root_dir,"Train.npz")):
+          if os.path.exists(os.path.join(self.config.root_dir,"Test.npz")):
+              
+              logging.info("Features ALREADY transformed")
+
+              transformed_data_path = self.config.root_dir
+              train_data = np.load(os.path.join(transformed_data_path, 'Train.npz'))
+              test_data = np.load(os.path.join(transformed_data_path, 'Test.npz'))
+
+              X_train, y_train, groups_train = train_data["X_train"], train_data["y_train"], train_data["groups_train"]
+              X_test, y_test = test_data["X_test"], test_data["y_test"]
+              
+              with open(self.config.STATUS_FILE,"w") as f:
+                f.write(f"Feature engineering completed {True}")
+              return X_train, X_test, y_train, y_test, groups_train
+          else:
+              return self.proceed_with_feature_engineering(X_train, X_test, y_train, y_test, groups_train)
+
+      else:
+          return self.proceed_with_feature_engineering(X_train, X_test, y_train, y_test, groups_train) 
+    
+      
     
     def save_transformed_data(self,X_train, X_test, y_train, y_test, groups_train):
         transformed_data_path = self.config.root_dir
