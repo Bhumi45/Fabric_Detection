29503855ed12a59e9cae46a8e20562241741f841
Parthsarthi Joshi <user.email>
2024-10-08 23:58:54 +0000

    minor changes

--- deployment/app.py
+++ deployment/app.py
@@ -122,3 +122,7 @@ def prediction():
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
