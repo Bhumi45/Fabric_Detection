51ffdfa0722a4289687b112fa793d9227f52c395
Parthsarthi Joshi <parthucmas@gmail.com>
2024-10-11 16:56:36 +0530

    s3 bucket code added

--- deployment/components/prediction.py
+++ deployment/components/prediction.py
@@ -1,34 +1,42 @@
 import numpy as np
-import pandas as pd
-import os
 import joblib
+import boto3
 import sys
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 from exception import CustomException
-
 
 class Prediction:
 
     def __init__(self):
-        pass
-        
+        self.s3_client = boto3.client('s3')
+        self.bucket_name = 'fabric-artifact-storage'  # Your S3 bucket name
+        self.model_key = 'artifacts/model_trainer/final_model.joblib'  # S3 key for the final model file
+        self.local_model_path = '/tmp/final_model.joblib'  # Temporary local path to save the model
+
+    def download_model_from_s3(self):
+        try:
+            # Download the model from S3 to a local path
+            self.s3_client.download_file(self.bucket_name, self.model_key, self.local_model_path)
+            print(f"Model downloaded from S3 and saved to {self.local_model_path}")
+        except Exception as e:
+            raise CustomException(f"Error downloading model: {str(e)}")
+
     def predict(self, transformed_features):
         try:
-            model_file_path = "artifacts/model_trainer/final_model.joblib"
-            model = joblib.load(model_file_path)
-            prediction  = model.predict(transformed_features)[0]
+            # Download the model from S3 before prediction
+            self.download_model_from_s3()
+
+            # Load the model from the local path
+            model = joblib.load(self.local_model_path)
+
+            # Perform prediction
+            prediction = model.predict(transformed_features)[0]
             print(prediction)
+
+            # Mapping prediction to category
             category_mapping = {1: 'corduroy', 2: 'denim', 3: 'linen'}
             predicted_label = category_mapping.get(prediction, "Unknown")
 
             return predicted_label
-        
+
         except Exception as e:
             raise CustomException(e, sys)
-        
-
-
-
-
