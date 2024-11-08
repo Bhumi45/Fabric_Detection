87da098eea3becf31f65119e69532ae82176c598
Parthsarthi Joshi <parthucmas@gmail.com>
2024-11-08 08:03:28 +0530

    pipeline executions updated

--- execute_cross_validation.py
+++ execute_cross_validation.py
@@ -3,50 +3,39 @@ from training.pipeline.common.data_validation import DataValidationPipeline
 from training.pipeline.common.feature_extraction import FeatureExtractionPipeline
 from training.pipeline.cross_val.nested_cross_val import NestedCrossValPipeline
 
-from logger import logging
-from exception import CustomException
+from training.custom_logging import info_logger, error_logger
 import sys
 import os
 
+
+
 PIPELINE = "Data Ingestion Training Pipeline"
-try:
-    logging.info(f">>>>> {PIPELINE} started <<<<")
-    data_ingestion = DataIngestionPipeline()
-    data_ingestion.main()
-    logging.info(f">>>>>>>> {PIPELINE} completed <<<<<<<<<")
-except Exception as e:
-    logging.error(e)
-    raise CustomException(e,sys)
+info_logger.info(f">>>>> {PIPELINE} started <<<<")
+data_ingestion = DataIngestionPipeline()
+data_ingestion.main()
+info_logger.info(f">>>>>>>> {PIPELINE} completed <<<<<<<<<")
+
 
 
 PIPELINE = "Data Validation Training Pipeline"
-try:
-    logging.info(f">>>>> {PIPELINE} started <<<<")
-    data_validation = DataValidationPipeline()
-    data_validation.main()
-    logging.info(f">>>>>>>> {PIPELINE} completed <<<<<<<<<")
-except Exception as e:
-    logging.error(e)
-    raise CustomException(e,sys)
+info_logger.info(f">>>>> {PIPELINE} started <<<<")
+data_validation = DataValidationPipeline()
+data_validation.main()
+info_logger.info(f">>>>>>>> {PIPELINE} completed <<<<<<<<<")
+
 
 PIPELINE = "Feature Extraction Training Pipeline"
-try:
-    logging.info(f">>>>> {PIPELINE} started <<<<")
-    feature_extraction = FeatureExtractionPipeline()
-    feature_extraction.main()
-    logging.info(f">>>>>>>> {PIPELINE} completed <<<<<<<<<")
-except Exception as e:
-    logging.error(e)
-    raise CustomException(e,sys)
+info_logger.info(f">>>>> {PIPELINE} started <<<<")
+feature_extraction = FeatureExtractionPipeline()
+feature_extraction.main()
+info_logger.info(f">>>>>>>> {PIPELINE} completed <<<<<<<<<")
+
 
 PIPELINE = "Nested Cross ValidationTraining Pipeline"
-try:
-    logging.info(f">>>>> {PIPELINE} started <<<<")
-    nested_cross_val = NestedCrossValPipeline()
-    nested_cross_val.main()
-    logging.info(f">>>>>>>> {PIPELINE} completed <<<<<<<<<")
-except Exception as e:
-    logging.error(e)
-    raise CustomException(e,sys)
+info_logger.info(f">>>>> {PIPELINE} started <<<<")
+nested_cross_val = NestedCrossValPipeline()
+nested_cross_val.main()
+info_logger.info(f">>>>>>>> {PIPELINE} completed <<<<<<<<<")
+
 
 
