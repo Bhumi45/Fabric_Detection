b46c1b5e5bf48308bc7cfae715db680507636e1b
Parthsarthi Joshi <parthucmas@gmail.com>
2024-11-08 07:58:26 +0530

    components updated with error and logging

--- training/components/common/data_validation.py
+++ training/components/common/data_validation.py
@@ -4,8 +4,8 @@ from PIL import Image
 from concurrent.futures import ThreadPoolExecutor  # Allows multi-threading
 from training.entity.config_entity import DataValidationConfig
 from training.configuration_manager.configuration import ConfigurationManager
-from logger import logging
-from exception import CustomException
+from training.exception import DataValidationError,handle_exception
+from training.custom_logging import info_logger, error_logger
 
 
 
@@ -50,7 +50,7 @@ class DataValidation:
                     file_paths.append(file_path)
 
             # Log the start of image validation
-            logging.info(f"Starting image validation for {len(file_paths)} files in {self.config.data_dir}")
+            info_logger.info(f"Starting image validation for {len(file_paths)} files in {self.config.data_dir}")
 
             # Multithreading to speed up the validation process
             with ThreadPoolExecutor(max_workers=num_threads) as executor:
@@ -59,7 +59,7 @@ class DataValidation:
             # Check the results and log non-image files
             for file_path, is_image in zip(file_paths, results):
                 if not is_image:
-                    logging.info(f"Non-image file found: {file_path}")
+                    info_logger.info(f"Non-image file found: {file_path}")
                     all_images = False
 
             # Write the result to the STATUS_FILE
@@ -67,19 +67,18 @@ class DataValidation:
             with open(self.config.STATUS_FILE, "w") as status_file:
                 status_file.write(status_message)
 
-            logging.info(status_message)
+            info_logger.info(status_message)
 
         except Exception as e:
-            logging.error(f"Error occurred during data validation: {str(e)}")
             with open(self.config.STATUS_FILE, "w") as status_file:
                 status_file.write(f"Data Validation failed due to an error: {str(e)}")
-            raise CustomException(e, sys)
+            handle_exception(e, DataValidationError)
 
 
 # To check the component
-"""if __name__ == "__main__":
+if __name__ == "__main__":
     conifg = ConfigurationManager()
     data_validation_config = conifg.get_data_validation_config()
 
     data_validation = DataValidation(data_validation_config)
-    data_validation.check_all_data_is_images()"""
+    data_validation.check_all_data_is_images()
